25dc748c6e31df578a09207fd60955c83f742b71
tapnguyen <nghuytap@gmail.com>
2014-09-15 13:36:52 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(window).load(function () {
     $('.flexslider').flexslider({ directionNav: false });
+    $('#galleryView').click(function(){
+    	$('#box').show();
+    	gallery();
+    });
+    $('#close').click(function(){
+		$('#box').hide();
+	})
 });
+
+
+function gallery() {
+    $("#gallery").carouFredSel({
+		responsive	: true,
+		prev        : ".left",
+	    next        : ".right"	
+	});
+	//$("#gallery").imagesLoaded(gallery);	
+
+}
